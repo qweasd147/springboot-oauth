e7c27a6a89a0bb7bb6501a31d3a7db1c5825a652
joohyung <qweasd_147@naver.com>
2019-12-04 14:30:17 +0900

    jwt resource 서버 인증 확인 api 추가

---
+++ jwt/resources-server/src/main/java/com/jwt/demo/api/UserController.java
@@ -0,0 +1,25 @@
+package com.jwt.demo.api;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    public Principal me(final Principal principal){
+        return principal;
+    }
+
+
+    @GetMapping
+    public ResponseEntity ok(){
+        return ResponseEntity.ok().build();
+    }
+}
