eae50447f93d59dc5e3090490a965dae334e5878
joohyung <qweasd_147@naver.com>
2019-12-11 10:14:00 +0900

    sql script 추가

---
+++ basic/authentication-server/src/main/resources/data-oauth.sql
@@ -0,0 +1,27 @@
+INSERT INTO `oauth_client_details`(
+  `client_id`,
+  `resource_ids`,
+  `client_secret`,
+  `scope`,
+  `authorized_grant_types`,
+  `web_server_redirect_uri`,
+  `authorities`,
+  `access_token_validity`,
+  `refresh_token_validity`,
+  `additional_information`,
+  `autoapprove`
+  )
+
+  VALUES(
+  'clientId',
+  null,
+  '{bcrypt}$2a$10$vCXMWCn7fDZWOcLnIEhmK.74dvK1Eh8ae2WrWlhr2ETPLoxQctN4.',
+  'read_profile,read_posts',
+  'authorization_code,implicit,password,client_credentials,refresh_token',
+  'http://localhost:9000/callback',
+  null,
+  3600,
+  86400,
+  null ,
+  'false'
+  );
